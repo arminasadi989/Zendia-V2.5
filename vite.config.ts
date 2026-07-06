7f6f3a74a2f7ae7a4d753977aebddf7eddbdb33d
arminasadi989 <arminasadi.989@gmail.com>
2026-07-06 17:13:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
+    alloWdhosts: 'zendia-v2-5.onrender.com', 
   },
   plugins: [react()],
   envPrefix: ['VITE_', 'GEMINI_'],
