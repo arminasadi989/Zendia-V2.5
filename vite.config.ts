739945e15166c91f7b125e64c7f1bad4c7ab3181
arminasadi989 <arminasadi.989@gmail.com>
2026-07-06 18:11:11 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: [
-      'zendia-v2-5.onrender.com'
-    ],
-  },
-
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'zendia-v2-5.onrender.com'
-    ],
-  },
-
   plugins: [react()],
 
   envPrefix: ['VITE_', 'GEMINI_'],
